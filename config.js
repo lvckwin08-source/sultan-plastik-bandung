07407488ddcdf7575e85e64e0016cedfe3f73fa7
lvckwin08-source <lvckwin08@gmail.com>
2026-06-05 11:09:49 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+// GOOGLE ANALYTICS 4
+(function(){
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-DGFE4D7J0P';
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-DGFE4D7J0P');
+})();
+
 /* ==========================================================
    SULTAN PLASTIK — CONFIG FILE
    ==========================================================
